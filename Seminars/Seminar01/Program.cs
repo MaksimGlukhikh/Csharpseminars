5789fad11251b0158c996a2cbe50018825a601cf
Maksim Glukhikh <qwer-87@mail.ru>
2023-09-18 12:36:26 +0500

    Добалены папки для семинара

---
+++ Seminars/Seminar01/Program.cs
@@ -0,0 +1,16 @@
+﻿//Console.Write("Введите число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Ваше число -> " + num*num);
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num2 * num2 == num1)
+{
+    Console.WriteLine("Да " );
+}
+else
+{
+    Console.WriteLine("нет ");
+}
