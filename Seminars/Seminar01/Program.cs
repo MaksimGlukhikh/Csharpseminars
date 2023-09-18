158cc0b77aa5416f2f00fc6af2adda9ec2979d59
Maksim Glukhikh <qwer-87@mail.ru>
2023-09-18 13:20:04 +0500

    Добален первый семинар

--- Seminars/Seminar01/Program.cs
+++ Seminars/Seminar01/Program.cs
@@ -2,15 +2,48 @@
 //int num = Convert.ToInt32(Console.ReadLine());
 //Console.WriteLine("Ваше число -> " + num*num);
 
-Console.Write("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-if (num2 * num2 == num1)
-{
-    Console.WriteLine("Да " );
-}
-else
-{
-    Console.WriteLine("нет ");
-}
+//Console.Write("Введите первое число: ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите второе число: ");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//if (num2 * num2 == num1)
+//{
+  //  Console.WriteLine("Да " );
+//}
+//else
+//{
+  //  Console.WriteLine("нет ");
+//}
+
+// Задача 3. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+//System.Console.WriteLine("Input number: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int index = - number;
+///while(index < number + 1)
+//{
+  //  System.Console.Write($" {index}, ");
+    //index++;
+//}
+
+// Задача 4.  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает **последнюю** цифру этого числа.
+    
+    // 456 -> 6 
+    
+    // 782 -> 2
+    
+    // 918 -> 8
+
+// System.Console.WriteLine("Введите число 3-х значное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(number>=100 && number <=999)
+// {
+// System.Console.Write($"{number} -> {number %10}");
+// }
+// else
+// {
+// System.Console.WriteLine("Вы ввели не коректное число");
+// }
