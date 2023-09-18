8ddfc4cd2d58f90c7c6e9e071ed2aada757dc8ed
Maksim Glukhikh <qwer-87@mail.ru>
2023-09-18 19:54:10 +0500

    Причесал код

--- Homeworks/Homeworks01/Program.cs
+++ Homeworks/Homeworks01/Program.cs
@@ -3,18 +3,18 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-// System.Console.Write("Input A: ");
-// int numberA = Convert.ToInt32(Console.ReadLine());
-// System.Console.Write("Input B: ");
-// int numberB = Convert.ToInt32(Console.ReadLine());
-// if(numberA > numberB)
-// {
-//     System.Console.Write($"max = {numberA}, min = {numberB}");
-// }
-// else
-// {
-//    System.Console.Write($"max = {numberB}, min = {numberA}"); 
-// }
+System.Console.Write("Input A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+{
+    System.Console.Write($"max = {numberA}, min = {numberB}");
+}
+else
+{
+   System.Console.Write($"max = {numberB}, min = {numberA}"); 
+}
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 // 2, 3, 7 -> 7
@@ -38,29 +38,29 @@ System.Console.Write($"max = {max}");
 // -3 -> нет
 // 7 -> нет
 
-// System.Console.Write("Input N: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-//  if(number %2 == 0)
-// {
-//     System.Console.Write($"Even number ");
-// }
-//  else
-//  {
-//     System.Console.Write($"The number is not even ");
-//  }
+System.Console.Write("Input N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ if(number %2 == 0)
+{
+    System.Console.Write($"Even number ");
+}
+ else
+ {
+    System.Console.Write($"The number is not even ");
+ }
 
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-// System.Console.Write("Input N: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-// int index = 1;
-// while(index <= number)
-// {
-//     if(index %2 == 0)
-//     {
-//     System.Console.Write($" {index}, ");
-//     }
-//     index++;
-// }
+System.Console.Write("Input N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while(index <= number)
+{
+    if(index %2 == 0)
+    {
+    System.Console.Write($" {index}, ");
+    }
+    index++;
+}
